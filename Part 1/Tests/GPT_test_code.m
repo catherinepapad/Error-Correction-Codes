98296aa19b756c8d4186af96aa01c566ec9d5290
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2023-12-01 20:20:17 +0200

    Add plots

--- Part 1/Tests/GPT_test_code.m
+++ Part 1/Tests/GPT_test_code.m
@@ -3,7 +3,7 @@ M = 4; % Modulation order
 SNR_dB = 10; % Signal-to-noise ratio in dB
 
 % Generate PAM signal
-data = randi([0, M-1], 10000, 1);
+data = randi([0, M-1], 100, 1);
 symbols = pammod(data, M);
 
 % Add noise
@@ -15,3 +15,17 @@ demodulated_data = pamdemod(noisy_symbols, M);
 % Evaluate performance
 bit_error_rate = biterr(data, demodulated_data) / numel(data);
 disp(['Bit Error Rate: ', num2str(bit_error_rate)]);
+
+% Plot Constellation
+figure;
+subplot(2,1,1);
+plot(symbols, 'bo'); % Original constellation
+title('PAM Constellation');
+
+% Plot Constellation with Noise
+subplot(2,1,2);
+plot(noisy_symbols, 'ro'); % Constellation with noise
+title('Noisy PAM Constellation');
+
+% Show plots
+sgtitle('PAM Constellation and Noisy Constellation');
