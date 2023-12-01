b4a8a392c34057ca75a5a534f34eddf1d9acfef5
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2023-12-01 21:28:43 +0200

    Add limits to axis

--- Part 1/Tests/GPT_test_code.m
+++ Part 1/Tests/GPT_test_code.m
@@ -1,10 +1,11 @@
+% close all
 % Parameters
 M = 4; % Modulation order
-SNR_dB = 10; % Signal-to-noise ratio in dB
-initial_phase = pi/4; % Initial phase in radians
+SNR_dB = 25; % Signal-to-noise ratio in dB
+initial_phase = 0; % Initial phase in radians
 
 % Generate Gray-coded PAM signal with specified initial phase
-gray_data = randi([0, M-1], 1000, 1);
+gray_data = randi([0, M-1], 10^3, 1);
 binary_data = de2bi(gray_data, log2(M), 'left-msb'); % Convert to binary
 symbols = pammod(gray_data, M, initial_phase, 'gray');
 
@@ -25,11 +26,17 @@ disp(['Bit Error Rate: ', num2str(bit_error_rate)]);
 figure;
 subplot(2,1,1);
 plot(real(symbols), imag(symbols), 'bo'); % Original constellation
+xlim([-40 40]);
+axis equal
 title('Gray-coded PAM Constellation');
 
 % Plot Constellation with Noise
 subplot(2,1,2);
 plot(real(noisy_symbols), imag(noisy_symbols), 'ro'); % Constellation with noise
+hold on ;
+plot(real(symbols), imag(symbols), 'bx'); % Original constellation
+xlim([-40 40]);
+axis equal
 title('Noisy Gray-coded PAM Constellation');
 
 % Show plots
