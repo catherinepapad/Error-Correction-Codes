a79ef57dc9da36318efef8e6b0bdabaf6d6f1496
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2023-12-31 18:19:35 +0200

    Add noise and ploting

--- Part 1/Tests/Test_normalized_energy.m
+++ Part 1/Tests/Test_normalized_energy.m
@@ -1,8 +1,8 @@
 % Parameters
 close all ; 
-
+SNR_dB = 10 ; 
 symbols_send = 10^3 ; 
-bits_per_symbol = 5 ;   
+bits_per_symbol = 4 ;   
 M = 2^bits_per_symbol; % Order of modulation (e.g., 16 for 16-QAM)
 
 symbols = randi([0 M-1], symbols_send, 1); % Generate random symbols
@@ -16,12 +16,42 @@ constalletion_energy = mean(abs(constalletion_points).^2) ;
 disp(['Constalletion mean energy: ', num2str(constalletion_energy)])
 
 
+
+% ============ Start of simulation ============
+
+% Modulate the symbols
 modulated_signal = qammod(symbols, M, 'UnitAveragePower', useUnitAveragePower);
 
+
+% Adding AWGN
+noisy_symbols = awgn(modulated_signal, SNR_dB, 'measured');
+
 % Demodulation
-demodulated_signal = qamdemod(modulated_signal, M, 'UnitAveragePower', useUnitAveragePower);
+demodulated_signal = qamdemod(noisy_symbols, M, 'UnitAveragePower', useUnitAveragePower);
 
 
 % Compare the original and demodulated symbols
 disp(['SER:'  num2str(sum(symbols~=demodulated_signal) * 100 / symbols_send) '%'])
+
+
+
+% Plot Constellation
+figure;
+subplot(2,1,1);
+plot(real(constalletion_points), imag(constalletion_points), 'bo'); % Original constellation
+% xlim([-40 40]);
+axis equal
+title('Gray-coded QAM Constellation');
+
+% Plot Constellation with Noise
+subplot(2,1,2);
+plot(real(noisy_symbols), imag(noisy_symbols), 'ro'); % Constellation with noise
+hold on ;
+plot(real(constalletion_points), imag(constalletion_points), 'bx'); % Original constellation
+% xlim([-40 40]);
+axis equal
+title('Noisy Gray-coded QAM Constellation');
+
+% Show plots
+sgtitle('Gray-coded QAM Constellation and Noisy Constellation');
 
