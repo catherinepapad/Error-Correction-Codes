108a0f335d02f34ac350c41ad270d164bd775720
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2023-12-31 18:00:30 +0200

    Fix constalletion energy calculation and a print

--- Part 1/Tests/Test_normalized_energy.m
+++ Part 1/Tests/Test_normalized_energy.m
@@ -1,5 +1,6 @@
 % Parameters
 close all ; 
+
 symbols_send = 10^3 ; 
 bits_per_symbol = 5 ;   
 M = 2^bits_per_symbol; % Order of modulation (e.g., 16 for 16-QAM)
@@ -11,7 +12,7 @@ useUnitAveragePower = true; % Set to false if you don't want unit average power
 
 % Calculate the mean energy of the constalletion
 constalletion_points = qammod(0:M-1 , M ,'UnitAveragePower', useUnitAveragePower,'PlotConstellation',true); 
-constalletion_energy = sqrt(mean(abs(constalletion_points).^2)) ; 
+constalletion_energy = mean(abs(constalletion_points).^2) ; 
 disp(['Constalletion mean energy: ', num2str(constalletion_energy)])
 
 
@@ -21,7 +22,5 @@ modulated_signal = qammod(symbols, M, 'UnitAveragePower', useUnitAveragePower);
 demodulated_signal = qamdemod(modulated_signal, M, 'UnitAveragePower', useUnitAveragePower,'NoiseVariance',100);
 
 % Compare the original and demodulated symbols
-disp(['SER:'  sum(symbols~=demodulated_signal) * 100 / symbols_send '%'])
-disp(sum(symbols~=demodulated_signal) * 100 / symbols_send)
-disp(sum(symbols~=demodulated_signal))
+disp(['SER:'  num2str(sum(symbols~=demodulated_signal) * 100 / symbols_send) '%'])
 
