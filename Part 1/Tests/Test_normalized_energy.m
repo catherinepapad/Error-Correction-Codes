79808c0aba0e5cd54477eae4c614ebcd8f41d984
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2023-12-31 18:58:54 +0200

    Add small file to make a SER and BER graph vs SNR to run you need to comment some things in the other file

--- Part 1/Tests/Test_normalized_energy.m
+++ Part 1/Tests/Test_normalized_energy.m
@@ -1,9 +1,11 @@
 % Parameters
 close all ; 
-SNR_dB = 20 ; 
-symbols_send = 10^3 ; 
-bits_per_symbol = 4 ;   
-M = 2^bits_per_symbol; % Order of modulation (e.g., 16 for 16-QAM)
+SNR_db = 15 ;           % SNR in db
+symbols_send = 10^5 ;   % Number of symbols to send
+bits_per_symbol = 4 ;   % Order of modulation (e.g., bits_per_symbol=4 thus M=16 for 16-QAM)
+M = 2^bits_per_symbol;  % Order of modulation (e.g., 16 for 16-QAM)
+
+make_plots = false ; 
 
 symbols = randi([0 M-1], symbols_send, 1); % Generate random symbols
 
@@ -11,9 +13,9 @@ symbols = randi([0 M-1], symbols_send, 1); % Generate random symbols
 useUnitAveragePower = true; % Set to false if you don't want unit average power
 
 % Calculate the mean energy of the constalletion
-constalletion_points = qammod(0:M-1 , M ,'UnitAveragePower', useUnitAveragePower,'PlotConstellation',true); 
-constalletion_energy = mean(abs(constalletion_points).^2) ; 
-disp(['Constalletion mean energy: ', num2str(constalletion_energy)])
+constellation_points = qammod(0:M-1 , M ,'UnitAveragePower', useUnitAveragePower,'PlotConstellation',make_plots); 
+constellation_energy = mean(abs(constellation_points).^2) ; 
+% disp(['Constalletion mean energy: ', num2str(constellation_energy)])
 
 
 
@@ -24,24 +26,30 @@ modulated_signal = qammod(symbols, M, 'UnitAveragePower', useUnitAveragePower);
 
 
 % Adding AWGN
-noisy_symbols = awgn(modulated_signal, SNR_dB, 'measured');
+noisy_symbols = awgn(modulated_signal, SNR_db, 'measured'); % The  'measured' is not needed if we normalize the constellation
 
 % Demodulation
 demodulated_signal = qamdemod(noisy_symbols, M, 'UnitAveragePower', useUnitAveragePower);
 
 
+
 % Compare the original and demodulated symbols
-disp(['SER: '  num2str(sum(symbols~=demodulated_signal) * 100 / symbols_send) '%'])
+[~,SER] = symerr(symbols,demodulated_signal) ;
+[~,BER] = biterr(symbols,demodulated_signal) ;
+disp(['SER: '  num2str(100*SER) '%'])
+disp(['BER: '  num2str(100*BER) '%'])
 
 
+% ============ END of simulation ============
 
-% Plot only the Constellation with Noise
-figure; 
-plot(real(noisy_symbols), imag(noisy_symbols), 'ro'); % Constellation with noise
-hold on ;
-plot(real(constalletion_points), imag(constalletion_points), 'bx'); % Original constellation
-axis equal
-title( sprintf('Noisy Gray-coded QAM Constellation SNR: %.2f db', SNR_dB) );
-
+if make_plots
+    % Plot only the Constellation with Noise
+    figure; 
+    plot(real(noisy_symbols), imag(noisy_symbols), 'ro'); % Constellation with noise
+    hold on ;
+    plot(real(constellation_points), imag(constellation_points), 'bx'); % Original constellation
+    axis equal
+    title( sprintf('Noisy Gray-coded QAM constellation SNR: %.2f db', SNR_db) );
+end
 
 
