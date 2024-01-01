0959ba38ff0286ac70ad5901395ce0a8e1278819
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2024-01-01 02:00:23 +0200

    Add an ideas file, with useful functions and suggestions

---
+++ Part 1/Ideas_usfull_funtions.m
@@ -0,0 +1,12 @@
+
+% Ideas
+
+
+
+
+% syndtable %Produce syndrome decoding table
+
+
+% Use this in the "generatePMatrix" function to get better/faster results
+% Perform row operations to maximize minimum Hamming distance
+% P = rref(P);  % This might also be a fancy idea
