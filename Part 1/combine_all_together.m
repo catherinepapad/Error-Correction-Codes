72ee780b2c2520d40e7e79f712aa301acd08584c
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2024-01-04 01:39:33 +0200

    Make plots better and factor them out

--- Part 1/combine_all_together.m
+++ Part 1/combine_all_together.m
@@ -201,36 +201,38 @@ for bits_per_symbol_index = 1:length(bits_per_symbol_array)
 end
 
 
+plot_resutls;
 
-% Do some plots !!!!!!!!!
-
-% Plotting BER with and without ECC on subplots with logarithmic scale and markers
-for i = 1:length(bits_per_symbol_array)
-    figure;
-    for j = 1:length(n_array)
-        subplot(length(n_array), 1, j);
-        semilogy(SNR_db_array, squeeze(ALL_BER_with_ECC(:, i, j)), 'o-', 'DisplayName', 'BER with ECC');
-        hold on;
-        semilogy(SNR_db_array, squeeze(ALL_BER_non_ECC(:, i, j)), 'o-', 'DisplayName', 'BER without ECC');
-        title(['BER Comparison - bps: ', num2str(bits_per_symbol_array(i)), ', n: ', num2str(n_array(j))]);
-        xlabel('SNR (dB)');
-        ylabel('BER');
-        legend;
-        grid on;
-    end
-end
-
-% Plotting Code Rates on the same graph for all bits per symbol values with markers
-figure;
-for i = 1:length(bits_per_symbol_array)
-    plot(n_array, ALL_Rb_code(i, :), 'o-', 'DisplayName', ['bps: ', num2str(bits_per_symbol_array(i))]);
-    hold on;
-end
-title('Code Rate Comparison');
-xlabel('n');
-ylabel('Code Rate');
-legend;
-grid on;
+% 
+% % Do some plots !!!!!!!!!
+% 
+% % Plotting BER with and without ECC on subplots with logarithmic scale and markers
+% for i = 1:length(bits_per_symbol_array)
+%     figure;
+%     for j = 1:length(n_array)
+%         subplot(length(n_array), 1, j);
+%         semilogy(SNR_db_array, squeeze(ALL_BER_with_ECC(:, i, j)), 'o-', 'DisplayName', 'BER with ECC');
+%         hold on;
+%         semilogy(SNR_db_array, squeeze(ALL_BER_non_ECC(:, i, j)), 'o-', 'DisplayName', 'BER without ECC');
+%         title(['BER Comparison - bps: ', num2str(bits_per_symbol_array(i)), ', n: ', num2str(n_array(j))]);
+%         xlabel('SNR (dB)');
+%         ylabel('BER');
+%         legend;
+%         grid on;
+%     end
+% end
+% 
+% % Plotting Code Rates on the same graph for all bits per symbol values with markers
+% figure;
+% for i = 1:length(bits_per_symbol_array)
+%     plot(n_array, ALL_Rb_code(i, :), 'o-', 'DisplayName', ['bps: ', num2str(bits_per_symbol_array(i))]);
+%     hold on;
+% end
+% title('Code Rate Comparison');
+% xlabel('n');
+% ylabel('Code Rate');
+% legend;
+% grid on;
 
 
 
