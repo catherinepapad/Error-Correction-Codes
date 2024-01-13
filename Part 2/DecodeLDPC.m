ecbe57d857893a9efc99490419b8e74d9fbdc57b
Panos Syskakis <75637426+Pansysk75@users.noreply.github.com>
2024-01-13 18:37:19 +0200

    Add DecodeLDPC

---
+++ Part 2/DecodeLDPC.m
@@ -0,0 +1,174 @@
+function x = DecodeLDPC(H, y, max_iter, interactive)
+    % DecodeLDPC - Decoder for irregular LDPC codes in the binary erasure channel
+    % 
+    %   x = DecodeLDPC(H, y, max_iter, interactive) decodes the received vector y
+    %   using the parity check matrix H. The decoding is done using an iterative
+    %   message passing algorithm. The algorithm is stopped if either the algorithm
+    %   converges or the maximum number of iterations is reached.
+    %   If interactive is true, the decoding process is visualized in a GUI.
+    %
+    % Parameters:
+    %   H: parity check matrix
+    %   y: received vector (erasures are represented by NaNs)
+    %   max_iter: maximum number of iterations
+    %   interactive: if true, allows interactive stepping through iterations
+    % 
+    % Output:
+    %   x: decoded vector
+    %    If decoding is successful, x is a vector of 0s and 1s.
+    %    If decoding fails, x is NaN.
+    % 
+    % Example:
+    %   H = [1 0 1 0 1 0 0 0 0 0 0 0;
+    %        0 1 0 1 0 1 0 0 0 0 0 0;
+    %        0 0 0 0 0 0 1 0 1 0 1 0;
+    %        0 0 0 0 0 0 0 1 0 1 0 1];
+    %   y = [NaN 1 NaN 0 NaN 1 1 NaN NaN NaN NaN NaN];
+    %   x = DecodeLDPC(H, y, 10, true);
+    %   % x = [0 1 0 0 0 1 1 0 1 0 1 0]
+
+    % Initialization
+    [m, n] = size(H);
+
+    % If visualizing, keep history of y values
+    if interactive
+        y_history = zeros(max_iter, n);  
+        y_history(1, :) = y;
+    end
+
+    % Decoding iterations
+    for iter = 1:max_iter
+        %  Do one iteration of decoding
+        [y_next, has_updated] = decodeIteration(H, y);
+        y = y_next;
+        
+        % If no updates, then decoding is done
+        if ~has_updated
+            break;
+        end
+
+        % Update history
+        if interactive
+            y_history(iter+ 1 , :) = y;
+        end
+    end
+
+    if interactive
+        % Truncate y_history to the number of iterations
+        y_history = y_history(1:iter, :);
+        % Visualize
+        createLDPCDecodingPlot(H, y_history);
+    end
+
+    % Final output
+    if sum(isnan(y_history(iter, :))) > 0
+        x = NaN;
+    else
+        x = y_history(iter, :);
+    end
+end
+
+
+% Function to do single iteration of decoding
+function [y_next, has_updated ] = decodeIteration(H, y)
+    [m, n] = size(H);
+    has_updated = false;
+    y_next = y;
+
+    % Iterate over check nodes (rows of H)
+    for i = 1:m
+        edges = find(H(i, :) == 1);
+        % If only one incoming erased message (NaN), then set the outgoing
+        % message to mod-2 sum of the incoming messages
+        idx = isnan(y(edges));
+        if sum(idx) == 1
+            y_next(edges(idx)) = mod(sum(y(edges(~idx))), 2);
+            has_updated = true;
+        end
+    end
+end
+
+
+function createLDPCDecodingPlot(H, y_history)
+    [m, n] = size(H);
+    [G, variableNodes, checkNodes] = createBipartiteGraph(H);
+
+    current_iter = 1;
+    max_iter = size(y_history, 1);
+
+    % Setup the figure and GUI elements
+    fig = figure(1);
+    clf(fig);
+    
+    prevButton = uicontrol(fig, 'Style', 'pushbutton', 'String', 'Previous', 'Position', [20 20 50 20], 'Callback', @prevCallback);
+    nextButton = uicontrol(fig, 'Style', 'pushbutton', 'String', 'Next',    'Position', [100 20 50 20], 'Callback', @nextCallback);
+
+    % Callback functions
+    function prevCallback(~,~)
+        if current_iter > 1
+            current_iter = current_iter - 1;
+            plotGraph(current_iter);
+        end
+    end
+    function nextCallback(~,~)
+        if current_iter < max_iter
+            current_iter = current_iter + 1;
+            plotGraph(current_iter);
+        end
+    end
+
+    % Initial Plot
+    plotGraph(current_iter);
+
+    % Function to plot the graph
+    function plotGraph(iter)
+        y = y_history(iter, :);
+        % Manually calculate x-y coordinates and create the plot
+        yData = [(0:n-1)/(n-1), (0:m-1)/(m-1)];
+        xData = [zeros(1, n), ones(1, m)];
+        h = plot(G, 'XData', xData, 'YData', yData)
+        title(sprintf('LDPC Decoding - Iteration %d', iter-1));
+        
+        % Iterate over variable nodes to add colors and line styles
+        for i = 1:n
+            % Get node number in the graph
+            node = variableNodes(i);
+            % Get all edges connected to this node
+            edges = outedges(G, node);
+            if isnan(y(i))
+                % Erased node: red, dashed edges
+                highlight(h, node, 'NodeColor', [1 0 0]);
+                highlight(h, 'Edges', edges, 'LineStyle', '--');
+            elseif y(i) == 0
+                % y==0: thin gray edges
+                highlight(h, 'Edges', edges, 'LineWidth', 1, 'EdgeColor', [0.5 0.5 0.5]);
+            else
+                % y==1: thick black edges
+                highlight(h, 'Edges', edges, 'LineWidth', 1.5, 'EdgeColor', [0 0 0]);
+            end
+        end
+
+        % Label variable nodes with their values
+        h.NodeLabel(1:n) = arrayfun(@(x) sprintf('%d', x), y, 'UniformOutput', false);
+        h.NodeLabel(n+1:n+m) = arrayfun(@(x) sprintf('C%d', x), 1:m, 'UniformOutput', false);
+        
+        drawnow;
+    end
+end
+
+function [G, variableNodes, checkNodes] = createBipartiteGraph(H)
+    % Create a bipartite graph from the parity check matrix H
+    % Returns the graph, and the variable and check nodes' indices
+    [m, n] = size(H);
+    G = graph();
+    variableNodes = 1:n;
+    checkNodes = n+1:n+m;
+    G = addnode(G, n+m);
+    for i = 1:m
+        for j = 1:n
+            if H(i,j) == 1
+                G = addedge(G, checkNodes(i), variableNodes(j));
+            end
+        end
+    end
+end
