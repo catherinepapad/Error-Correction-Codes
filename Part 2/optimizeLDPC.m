23a6832761bf487f9ec1fb1d7a61423b9829f46e
catherinepapad <katerinapapado.01@gmail.com>
2024-01-15 14:04:15 +0200

    Fix optimization algorithm

--- Part 2/optimizeLDPC.m
+++ Part 2/optimizeLDPC.m
@@ -4,9 +4,6 @@ function [rho,lambda] = optimizeLDPC(r,r_avg,l_max,epsilon,interval)
     rho(2) = r*(r+1-r_avg)/r_avg;
     rho(1) = (r_avg - r*(r+1-r_avg))/r_avg;
     
-    % Polynomial lambda(x)
-    lambda = zeros(l_max-1,1);
-    
     
     %% Optimization problem for finding optimal lambda(x)
     % Objective function
@@ -21,34 +18,33 @@ function [rho,lambda] = optimizeLDPC(r,r_avg,l_max,epsilon,interval)
     beq = 1;
     
     % Constraint 3
-    A = constraint3(epsilon,rho,l_max,interval);
+    [A,b] = constraint3(epsilon,rho,l_max,interval);
 
     % Constraint 2
     A_size = size(A);
     for i=1:l_max-1
         A(A_size(1)+i,i)=-1;
-%         A(i,i)=-1;
+        b(A_size(1)+i) = 0;
     end
     
-    % b matrix
-    A_size = size(A);
-    b = zeros(1,A_size(1));
-    
-    % No bounds on decision variables
-    lb = [];
-    ub = [];
+    % Bounds on decision variables
+%     lb = [];
+    lb = zeros(1,l_max-1);
+%     ub = [];
+    ub = ones(1,l_max-1);
     
     % Solve with linprog
-%     x = linprog(f, A, b, Aeq, beq, lb, ub);
-    x = linprog(f, A, b, Aeq, beq);
+    lambda = linprog(f, A, b, Aeq, beq, lb, ub);
     disp('Optimal solution:');
-    disp(x);
+    disp(lambda);
+    
+    rho = flip(rho);
     
 end
 
 
 %% FUnction for contraint No3
-function A = constraint3(epsilon,rho,l_max,interval)
+function [A,b] = constraint3(epsilon,rho,l_max,interval)
     % Split [0,1] to equal parts by interval
     x = [];
     for i=0:interval:1  
@@ -59,8 +55,7 @@ function A = constraint3(epsilon,rho,l_max,interval)
     length = 1/interval + 1;
     
     if (length < 4)
-        disp('Insufficient discretization')
-        return
+        error('Insufficient discretization')
     else
         x = x(:,2:length-1);
         length = length  - 2;
@@ -74,11 +69,14 @@ function A = constraint3(epsilon,rho,l_max,interval)
    
     % Initialize contraint matrix A
     A = zeros((l_max-1)*length,l_max-1);
+    b = zeros(1,(l_max-1)*length);
     
     % Fill A
     for i=1:l_max-1
             for j=1:length 
-                A(j+length*(i-1),i) = epsilon*(1-subs(symbolic_rho, y, 1-x(j)))^i;
+%                 A(j+length*(i-1),i) = epsilon*(1-subs(symbolic_rho, y, 1-x(j)))^i;
+                A(j+length*(i-1),i) = (1-subs(symbolic_rho, y, 1-x(j)))^i;
+                b(j+length*(i-1)) = x(j) / epsilon;
             end
     end
 end
