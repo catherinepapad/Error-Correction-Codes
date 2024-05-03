ba9cc0d9c8c9bcd08e04a3604619d34fd56f719a
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2024-05-04 02:19:01 +0300

    Rewrite code to generalize and add some plots

--- Part 2/comparison_sweep.m
+++ Part 2/comparison_sweep.m
@@ -2,45 +2,72 @@ close all
 clear
 clc
 
-% define best LDPC for epsilon=0.2 
-Lambda1 = [0 1323 177];
-Rho1 = [0 0 0 0 0 1 453];
-[H1, G1] = createLdpcFromPoly(Lambda1, Rho1);
 
-% define best LDPC for epsilon=0.4
-Lambda2 = [0 1015 485];
-Rho2 = [0 0 0 0 697];
-[H2, G2] = createLdpcFromPoly(Lambda2, Rho2);
+% Define LDPC configurations for different epsilon values
+ldpc_configurations = {
+    struct('Lambda', [0 1323 177], 'Rho', [0 0 0 0 0 1 453],'epsilon', 0.2)  
+    struct('Lambda', [0 1323 177], 'Rho', [0 1 0 0 635],    'epsilon', 0.3)       
+    struct('Lambda', [0 1015 485], 'Rho', [0 0 0 0 697],    'epsilon', 0.4)       
+};
 
-epsilon_list = [0.01 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5];
-% epsilon_list = [0.1 0.3];
+epsilon_list = logspace(-2, log10(0.5), 50);
+sim_iterations = 100;
 
-sim_iterations = 50;
+num_ldpc = length(ldpc_configurations);
 
-irregular_erasure_rates = zeros(2,length(epsilon_list));
-irregular_failure_rates = zeros(2,length(epsilon_list));
+irregular_erasure_rates = zeros(num_ldpc, length(epsilon_list));
+irregular_failure_rates = zeros(num_ldpc, length(epsilon_list));
 
-for i = 1:length(epsilon_list)
-    epsilon = epsilon_list(i)
-    [irregular_erasure_rates(1,i), irregular_failure_rates(1,i)] = simulateLdpc(H1, G1, epsilon, sim_iterations);
-    [irregular_erasure_rates(2,i), irregular_failure_rates(2,i)] = simulateLdpc(H2, G2, epsilon, sim_iterations);
+
+% Simulation loop for each LDPC configuration
+for ldpc_index = 1:num_ldpc
+    Lambda = ldpc_configurations{ldpc_index}.Lambda;
+    Rho = ldpc_configurations{ldpc_index}.Rho;
+    ldpc_epsilon = ldpc_configurations{ldpc_index}.epsilon
+
+    % Create LDPC matrix and generator matrix
+    [H, G] = createLdpcFromPoly(Lambda, Rho);
+
+    % Simulation loop over epsilon values
+    for i = 1:length(epsilon_list)
+        epsilon = epsilon_list(i);
+        
+        % Simulate LDPC for current configuration and epsilon
+        [erasure_rate, failure_rate] = simulateLdpc(H, G, epsilon, sim_iterations);
+        
+        % Store results in respective arrays
+        irregular_erasure_rates(ldpc_index, i) = erasure_rate;
+        irregular_failure_rates(ldpc_index, i) = failure_rate;
+    end
+
+
 end
 
-% figure;
-% semilogy(epsilon_list(:), irregular_erasure_rates(1,:));
-% hold on
-% semilogy(epsilon_list(:), irregular_erasure_rates(2,:));
-% xlabel('Channel Erasure Probability');
-% ylabel('BER');
-% title('BER comparison for 2 different  LDPC codes');
-% legend('$\epsilon$=0.2','$\epsilon$=0.4','interpreter','latex');
+% Legend in latex format
+legends_string = arrayfun( @(ldpc_index) sprintf("$\\epsilon$=%.1f", ldpc_configurations{ldpc_index}.epsilon) , 1:num_ldpc) ;
 
+% Log scale 
 figure;
-plot(epsilon_list(:), irregular_erasure_rates(1,:));
 hold on
-plot(epsilon_list(:), irregular_erasure_rates(2,:));
+set(gca, 'YScale', 'log');
+for ldpc_index = 1:num_ldpc
+    plot(epsilon_list, irregular_erasure_rates(ldpc_index,:));
+end
 xlabel('Channel Erasure Probability');
 ylabel('BER');
-title('BER comparison for 2 different  LDPC codes');
-legend('$\epsilon$=0.2','$\epsilon$=0.4','interpreter','latex');
+title('BER comparison for different  LDPC codes');
+legend(legends_string,'interpreter','latex','Location','best');
+
+
+% Log scale 
+figure;
+hold on
+for ldpc_index = 1:num_ldpc
+    plot(epsilon_list, irregular_erasure_rates(ldpc_index,:));
+end
+xlabel('Channel Erasure Probability');
+ylabel('BER');
+title('BER comparison for different  LDPC codes');
+legend(legends_string,'interpreter','latex','Location','best');
+
 
